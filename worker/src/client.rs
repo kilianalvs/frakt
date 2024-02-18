e526aa5592417f2957fb313e78e1d7a37f1e5a6e
julien <julien.riviere94120@gmail.com>
2024-02-18 17:37:04 +0100

    feat : écriture d'un client TCP hello world dans client.rs, TO UPDATE

--- worker/src/client.rs
+++ worker/src/client.rs
@@ -3,16 +3,48 @@
 
 pub mod client{
     
-    use std::{io::{Read, Write}, net::TcpStream};
+    use std::{default, io::{Read, Write}};
+    use std::net::{TcpListener, TcpStream};
 
     use hostname::get;
     use serde::Serialize;
     use serde_json::{json, Value};
     use shared::messages_types::{FragmentRequest, FragmentResult, FragmentTask};
+    use debug_print::{debug_println}
 
     use crate::message::Message;
     
-    
+
+    //main client function
+    fn handle_client(mut stream : TcpStream){
+        let mut buffer = [0;1024];  //to read data from the client
+        stream.read(&mut buffer)//read data from the stream and store it in the buffer
+            .expect("Failed to read from client !");
+
+        let request = String::from_utf8_lossy(&buffer[..]); //buffer => utf8 encoded string
+        debug_println!("Received Request : {}", request); //debug only
+        let response = "hello client".as_bytes();
+        stream.write(response).expect("Failed to write response!");
+    }
+
+    //entry point
+    fn main(){
+        let target_adress: &str = "127.0.0.1:16142";
+        let listener = TcpListener::bind(target_adress).expect("failed to bind to addresse");
+        debug_println!("server is listening on {}", target_adress);
+
+        for stream in listener.incoming(){
+            match stream {
+                Ok(stream) =>{
+                    std::thread::spawn(|| handle_client(stream));
+                }
+                Err(e) =>{
+                    eprintln!("failed to established connexion: {}",e);
+                    //stderr - standart error stream
+                }
+            }
+        }
+    }
 
 
     // Fonction pour envoyer un message JSON sur le flux TCP
@@ -28,18 +60,43 @@ pub mod client{
             }
             Err(err) => {
                 // Gérer l'erreur de sérialisation ici
-                eprintln!("Error serializing message: {:?}", err);
+                panic!("Error serializing message: {:?}", err);
+            }
+            default =>{
+                panic!("Something wrong happed: {:?}", default);
             }
         }
     }
 
     // Fonction pour recevoir un message JSON du flux TCP
-    fn receive_message(stream: &dyn Message) ->  Box<dyn Message> {
+    fn receive_message(stream: &TcpStream) -> Box<dyn Message> {
         let mut buffer = String::new();
-        let mut stream_clone = stream.try_clone().expect("Unable to clone TcpStream");
+        let mut stream_clone = stream.clone();
         stream_clone.read_to_string(&mut buffer).expect("Failed to read from stream");
 
-        serde_json::from_str(&buffer).expect("Failed to deserialize message")
+        // Désérialiser le message JSON en fonction de son type
+        if buffer.contains("FragmentRequest") {
+            Box::new(serde_json::from_str::<FragmentRequest>(&buffer).expect("Failed to deserialize FragmentRequest"))
+        } else if buffer.contains("FragmentResult") {
+            Box::new(serde_json::from_str::<FragmentResult>(&buffer).expect("Failed to deserialize FragmentResult"))
+        } else if buffer.contains("FragmentTask") {
+            Box::new(serde_json::from_str::<FragmentTask>(&buffer).expect("Failed to deserialize FragmentTask"))
+        } else {
+            panic!("Unknown message type");
+        }
+    }
+
+    pub fn receive_fragment_task(stream: &TcpStream) -> Result<FragmentTask, Box<dyn std::error::Error>> {
+        let mut buffer: String = String::new();
+        let mut stream_clone: TcpStream = stream.try_clone()?;
+        stream_clone.read_to_string(&mut buffer)?;
+
+        if buffer.contains("FragmentTask") {
+            let fragment_task: FragmentTask = serde_json::from_str(&buffer)?;
+            Ok(fragment_task)
+        } else {
+            Err("Received message is not a FragmentTask".into())
+        }
     }
 
     // Fonction pour calculer un FragmentResult à partir d'une tâche de fragment
@@ -55,33 +112,42 @@ pub mod client{
     }
 
 
-    // Fonction principale du client qui gère la boucle de communication avec le serveur
     pub fn run_client(stream: TcpStream) {
-        //setupClient();
-
+        loop {
+            match run_client_once(stream.try_clone()) {
+                Ok(_) => break, // Si la fonction s'exécute avec succès, sortir de la boucle
+                Err(err) => {
+                    eprintln!("Error running client: {}", err);
+                    continue; // Redémarrer la boucle pour réessayer
+                }
+            }
+        }
+    }
+    
+    fn run_client_once(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
         // Envoyer un FragmentRequest au serveur
-        let fragment_request: FragmentRequest = FragmentRequest::new(
-                // hostname::get() as String,
-                "client de test".to_string(),
-                1000
+        let fragment_request = FragmentRequest::new(
+            // hostname::get() as String,
+            "client de test".to_string(),
+            1000,
         );
         send_message(&stream, &fragment_request);
-
+    
         // Attendre la réponse du serveur (FragmentTask)
-        let mut fragment_task = receive_message(&stream);
-        println!("Received FragmentTask: {:?}", fragment_task);
-
+        let fragment_task = receive_fragment_task(&stream)?;
+        debug_println!("Received FragmentTask: {:?}", fragment_task);
+    
         // Boucle principale du client
         loop {
             // Calculer les pixels du fragment et créer un FragmentResult
             let fragment_result = calculate_fragment_result(&fragment_task);
-
+    
             // Envoyer le FragmentResult au serveur
-            send_message(&stream, &fragment_result);
-
+            send_message(&stream, &fragment_result)?;
+    
             // Attendre la réponse du serveur (FragmentTask ou FragmentResult)
             let server_response = receive_message(&stream);
-            match server_response.get("FragmentTask") {
+            match server_response.try_into(FragmentTask).get("FragmentTask") {
                 Some(task) => {
                     // Nouvelle tâche du serveur
                     println!("Received FragmentTask: {:?}", task);
--- worker/src/message.rs
+++ worker/src/message.rs
@@ -1,24 +1,40 @@
+use std::any::Any;
+
 use serde::{de::value::Error, Serialize};
 use shared::messages_types::{FragmentRequest, FragmentResult, FragmentTask};
 //use shared::messages_types::{FragmentRequest, FragmentResult, FragmentTask};
 
 // Définir un trait pour représenter les messages
-pub trait Message : Serialize + Clone {
+// #[vtable]
+// #[repr(C)]
+// #[derive(Debug, Serialize, PartialEq, Deserialize, Clone, Copy)]
+//définir enum plutot que trait
+pub trait Message {
     fn serialize(&self) -> Result<String, serde_json::Error>;
 }
 
+// Implémenter le trait Any pour permettre le downcast dynamique
+impl dyn Message {
+    fn as_any(&self) -> &dyn Message {
+        self
+    }
+}
+
+// Implémenter le trait pour FragmentRequest
 impl Message for FragmentRequest {
     fn serialize(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string(self)
     }
 }
 
+// Implémenter le trait pour FragmentResult
 impl Message for FragmentResult {
     fn serialize(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string(self)
     }
 }
 
+// Implémenter le trait pour FragmentTask
 impl Message for FragmentTask {
     fn serialize(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string(self)
